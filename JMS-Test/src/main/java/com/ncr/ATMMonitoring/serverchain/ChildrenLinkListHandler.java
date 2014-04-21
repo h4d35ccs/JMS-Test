5576422134d40691e6cfb1a27053322030fa1326
ottoabreu <ottoabreu@gmail.com>
2014-04-21 18:28:42 +0200

    removed serializable list

--- JMS-Test/src/main/java/com/ncr/ATMMonitoring/serverchain/ChildrenLinkListHandler.java
+++ JMS-Test/src/main/java/com/ncr/ATMMonitoring/serverchain/ChildrenLinkListHandler.java
@@ -37,9 +37,6 @@ public class ChildrenLinkListHandler {
 
     private Set<String> childrenSubscribed = new HashSet<String>();
 
-    @Value("${jms.children.subscription.list.path:}")
-    private String serializableSystemPath;
-
     private static final String SUBSCRIPTION_LIST_FILENAME = "subscription_list.bin";
 
     public boolean isChildSubscribed(String childIpandPort) {
@@ -57,34 +54,10 @@ public class ChildrenLinkListHandler {
 	
 	this.childrenSubscribed.add(childIpandPort);
 	
-	this.saveListInDisk();
     }
 
-    private void saveListInDisk() {
-
-	if (!StringUtils.isEmpty(this.serializableSystemPath)) {
-	    this.writeInDiskList();
-	}
-    }
-
-    private void writeInDiskList() {
-
-	try (FileOutputStream fos = new FileOutputStream(getFileCompletePath());
-
-		ObjectOutputStream out = new ObjectOutputStream(fos);) {
-
-	    out.writeObject(this.childrenSubscribed);
-
-	} catch (FileNotFoundException e) {
-	    logger.warn(
-		    "it was not posible to write the subscription list in disk because the given path is not valid ",
-		    e);
-	} catch (IOException e) {
-	    logger.warn(
-		    "it was not posible to write the subscription list in disk because an IO Exception ",
-		    e);
-	}
-    }
+    
+   
 
     public void removeChildIpToSubscribedList(String childIpandPort) {
 	this.childrenSubscribed.remove(childIpandPort);
@@ -109,34 +82,4 @@ public class ChildrenLinkListHandler {
 	return childAddressAndPort;
     }
     
-    @PostConstruct
-    public void loadList(){
-	if(this.childrenSubscribed.isEmpty()){
-	   this.readListFromDisk(); 
-	}
-    }
-
-    @SuppressWarnings("unchecked")
-    private void readListFromDisk() {
-	try (FileInputStream fis = new FileInputStream(getFileCompletePath());
-		ObjectInputStream in = new ObjectInputStream(fis)) {
-
-	    this.childrenSubscribed = (Set<String>) in.readObject();
-
-	} catch (FileNotFoundException e) {
-	    logger.warn(
-		    "it was not posible to read the subscription list from disk (file not found), "
-		    + "check the path in the property file, "
-		    + "is a normal behaviour if this is the first run and the list is empty");
-	} catch (IOException e) {
-	    logger.warn("it was not posible to read the subscription list from disk due an IOException ",e);
-	} catch (ClassNotFoundException e) {
-	    // is imposible to get an ClassNotFound for Set<String>
-	    logger.warn("ClassNotFoundException ",e);
-	}
-    }
-
-    private String getFileCompletePath() {
-	return this.serializableSystemPath + SUBSCRIPTION_LIST_FILENAME;
-    }
 }
