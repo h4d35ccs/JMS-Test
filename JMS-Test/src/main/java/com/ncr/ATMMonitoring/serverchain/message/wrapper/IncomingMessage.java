77630a52b084fba95c19fda8c5a74671f585b3a2
otto abreu <ottoabreu@gmail.com>
2014-04-28 18:25:43 +0200

    added public void accept (visitor pattern implementation)

--- JMS-Test/src/main/java/com/ncr/ATMMonitoring/serverchain/message/wrapper/IncomingMessage.java
+++ JMS-Test/src/main/java/com/ncr/ATMMonitoring/serverchain/message/wrapper/IncomingMessage.java
@@ -3,23 +3,28 @@
  */
 package com.ncr.ATMMonitoring.serverchain.message.wrapper;
 
+import com.ncr.ATMMonitoring.serverchain.message.wrapper.visitor.WrapperVisitor;
+
 
 /**
  * @author Otto Abreu
- *
+ * 
  */
-public class IncomingMessage extends MessageWrapper{
-
-    public IncomingMessage(String message, int id) {
-	super(message, id);
-	
-    }
+public class IncomingMessage extends MessageWrapper {
 
     /**
      * 
      */
     private static final long serialVersionUID = -6457607990812054891L;
 
-    
-    
+    public IncomingMessage(String message, int id) {
+	super(message, id);
+
+    }
+
+    @Override
+    public void accept(WrapperVisitor visitor) {
+	visitor.visit(this);
+
+    }
 }
--- JMS-Test/src/main/java/com/ncr/ATMMonitoring/serverchain/message/wrapper/OutgoingMessage.java
+++ JMS-Test/src/main/java/com/ncr/ATMMonitoring/serverchain/message/wrapper/OutgoingMessage.java
@@ -1,17 +1,22 @@
 package com.ncr.ATMMonitoring.serverchain.message.wrapper;
 
+import com.ncr.ATMMonitoring.serverchain.message.wrapper.visitor.WrapperVisitor;
+
+
 
 public class OutgoingMessage extends MessageWrapper {
 
-    
     private static final long serialVersionUID = 1L;
-    
+
     public OutgoingMessage(String message, int id) {
 	super(message, id);
-	
+
     }
 
+    @Override
+    public void accept(WrapperVisitor visitor) {
+	visitor.visit(this);
 
-    
+    }
 
 }
