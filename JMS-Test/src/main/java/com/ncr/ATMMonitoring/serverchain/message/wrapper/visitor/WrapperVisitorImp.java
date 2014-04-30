13e1583967ee8e4ac8c1d9ade6c28ad05f14f230
otto abreu <ottoabreu@gmail.com>
2014-04-30 11:35:38 +0200

    included the use of strategyExecutor

--- JMS-Test/src/main/java/com/ncr/ATMMonitoring/serverchain/message/wrapper/visitor/WrapperVisitorImp.java
+++ JMS-Test/src/main/java/com/ncr/ATMMonitoring/serverchain/message/wrapper/visitor/WrapperVisitorImp.java
@@ -1,20 +1,13 @@
 package com.ncr.ATMMonitoring.serverchain.message.wrapper.visitor;
 
-import javax.annotation.Resource;
-
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import com.ncr.ATMMonitoring.serverchain.ChainLinkInformation;
-import com.ncr.ATMMonitoring.serverchain.NodePosition;
-import com.ncr.ATMMonitoring.serverchain.message.SpecificMessage;
-import com.ncr.ATMMonitoring.serverchain.message.specific.strategy.SpecifcMessageProcessStrategy;
-import com.ncr.ATMMonitoring.serverchain.message.specific.strategy.StrategyFactory;
+import com.ncr.ATMMonitoring.serverchain.message.specific.strategy.StrategyExecutor;
 import com.ncr.ATMMonitoring.serverchain.message.wrapper.IncomingMessage;
 import com.ncr.ATMMonitoring.serverchain.message.wrapper.MessageWrapper;
 import com.ncr.ATMMonitoring.serverchain.message.wrapper.OutgoingMessage;
-import com.ncr.ATMMonitoring.serverchain.topicactor.producer.GenericMessageProducer;
 
 /**
  * Concrete implementation of WrapperVisitor
@@ -27,14 +20,8 @@ import com.ncr.ATMMonitoring.serverchain.topicactor.producer.GenericMessageProdu
 @Component
 public class WrapperVisitorImp implements WrapperVisitor {
 
-    @Resource(name = "outgoingMessageProducer")
-    private GenericMessageProducer outgoingProducer;
-
-    @Resource(name = "incomingMessageProducer")
-    private GenericMessageProducer incomingProducer;
-
     @Autowired
-    private ChainLinkInformation chainLinkInformation;
+    private StrategyExecutor strategyExecutor;
 
     private static final Logger logger = Logger
 	    .getLogger(WrapperVisitorImp.class);
@@ -67,53 +54,8 @@ public class WrapperVisitorImp implements WrapperVisitor {
     }
 
     private void applyStrategy(MessageWrapper message) {
-	
-	SpecificMessage specificMessage = message.getSpecificMessage();
-	NodePosition nodePosition = this.chainLinkInformation.getNodePosition();
-	
-	SpecifcMessageProcessStrategy strategyToAply = this.getStrategyToApply(
-		nodePosition, specificMessage);
+	this.strategyExecutor.execute(message);
 
-	if (strategyToAply.canProcessSpecificMessage()) {
-
-	    this.processMessage(strategyToAply, message);
-
-	}
-    }
-
-    private void passMessageToNextNode(MessageWrapper message) {
-
-	if (message instanceof OutgoingMessage) {
-
-	    this.outgoingProducer.sendMessage(message);
-
-	} else if (message instanceof IncomingMessage) {
-
-	    this.incomingProducer.sendMessage(message);
-	}
-
-    }
-
-    private void processMessage(SpecifcMessageProcessStrategy strategyToAply,
-	    MessageWrapper message) {
-
-	strategyToAply.processSpecificMessage();
-
-	if (strategyToAply.passToOtherNode()) {
-	    logger.debug("is going to pass message visitor: "+strategyToAply.passToOtherNode());
-	    this.passMessageToNextNode(message);
-	}
-    }
-
-    private SpecifcMessageProcessStrategy getStrategyToApply(
-	    NodePosition nodePosition, SpecificMessage message) {
-
-	SpecifcMessageProcessStrategy strategy = StrategyFactory
-		.getStrategyForSpecificMessage(message);
-	
-	strategy.setupStrategy(nodePosition, message);
-
-	return strategy;
     }
 
 }
